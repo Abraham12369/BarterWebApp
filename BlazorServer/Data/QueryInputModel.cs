6814f10bd642788a5f92439bc15d951e7beaea38
Jack Chen <qiming110038@gmail.com>
2021-07-12 02:21:55 -0700

    working push

---
+++ BlazorServer/Data/QueryInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Data
+{
+    public class QueryInputModel
+    {
+        public string baseResource { get; set; }
+        public string t1Resource { get; set; }
+        public string t2Resource { get; set; }
+        public string location1 { get; set; }
+        public string location2 { get; set; }
+    }
+}
---
+++ DataLibrary/TradeRoute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary
+{
+    public class TradeRoute
+    {
+        public string Chain;
+        public string ChainID;
+        public string Location0;
+        public string Location1;
+        public string Location2;
+        public string Location3;
+        public string Location4;
+        public string Location5;
+        public string Item0;
+        public string Item1;
+        public string Item2;
+        public string Item3;
+        public string Item4;
+        public string Item5;
+        public string Item6;
+        public string SubPaste;
+        public string Amount;
+    }
+}
